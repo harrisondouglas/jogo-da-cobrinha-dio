f2c57d1351583f5d2b589443fb5a2c7d6dd3f94b
harrison <harridouglas@gmail.com>
2021-11-25 03:41:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let canvas =document.getElementsById("snake");
 let context =canvas.getContext ("2d");
+let box=32;
+
+function criarBG() {
+     context.fillStyle ="lightgreen";
+     context.fillRect (0,0, 16 *box, 16 * box);
+
+}
